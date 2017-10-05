fefb7399b8c75a42d091e9d16931c3d1cad80140
Jan <bachmann.heinsberg@gmail.com>
2017-10-05 22:39:31 +0200

    Another initial commit

---
+++ cmake-build-debug/CMakeFiles/Emoh_Socket_Client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Emoh_Socket_Client.dir/main.cpp.obj"
+  "Emoh_Socket_Client.pdb"
+  "Emoh_Socket_Client.exe"
+  "Emoh_Socket_Client.exe.manifest"
+  "libEmoh_Socket_Client.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Emoh_Socket_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
